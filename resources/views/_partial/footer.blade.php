9a1a9d97f0635e44085360d93a234c9d45f0a328
nopebri1994 <nopebri94jr@gmail.com>
2024-03-21 11:26:08 +0700

    menambahkan template admin lte dengan blade

---
+++ resources/views/_partial/footer.blade.php
@@ -0,0 +1,20 @@
+<footer class="main-footer">
+
+    <div class="float-right d-none d-sm-inline">
+        Anything you want
+    </div>
+
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
+</footer>
+</div>
+
+
+
+<script src="assets/adminlte/js/jquery.min.js"></script>
+
+<script src="assets/adminlte/js/bootstrap.bundle.min.js"></script>
+
+<script src="assets/adminlte/js/adminlte.min.js?v=3.2.0"></script>
+</body>
+
+</html>
