1ccc5cebdb6133afacef8061946bc6a56eea8397
nopebri1994 <nopebri94jr@gmail.com>
2024-08-30 10:25:35 +0700

    rename

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -15,7 +15,7 @@ class homeController extends Controller
         $count = karyawanModel::whereNull('km')->count();
         $metal = karyawanModel::where('idPerusahaan', 1)->whereNull('km')->count();
         $bagian = DB::table('bagian')->select('namaBagian')->where('departemen.idPerusahaan', 1)->join('departemen', 'departemen.id', 'bagian.idDepartemen')->orderBy('bagian.id')->get()->toArray();
-        $metalCount = DB::table('dataKaryawan')->select(DB::raw('count(*) as total,idBagian'))->whereNull('km')->whereNotNull('idBagian')->where('idDepartemen', 1)->groupBy('idBagian')->orderBy('idBagian')->get()->toArray();
+        $metalCount = DB::table('dataKaryawan')->select(DB::raw('count(*) as total,idBagian'))->whereNull('km')->whereNotNull('idBagian')->where('idPerusahaan', 1)->groupBy('idBagian')->orderBy('idBagian')->get()->toArray();
         $data = [
             'title' => 'Sistem Manajemen Data Human Capital',
             'countEmployee' => $count,
