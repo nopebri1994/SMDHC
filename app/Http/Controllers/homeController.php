89b514105ba6336a88ef5d2f6bd288e486d2c9f1
nopebri1994 <nopebri94jr@gmail.com>
2024-09-05 13:07:13 +0700

    spmodel update

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -7,6 +7,7 @@ use App\Models\departemenModel;
 use Illuminate\Http\Request;
 use App\Models\karyawanModel;
 use App\Models\kontrakKaryawanModel;
+use App\Models\SPModel;
 use Illuminate\Support\Facades\DB;
 
 class homeController extends Controller
@@ -20,7 +21,7 @@ class homeController extends Controller
         $metal = karyawanModel::where('idPerusahaan', 1)->whereNull('km')->count();
         $metalCount = DB::table('dataKaryawan')->select(DB::raw('count(*) as total,idBagian,bagian.namaBagian'))->join('bagian', 'bagian.id', 'dataKaryawan.idBagian')->whereNull('km')->whereNotNull('idBagian')->where('idPerusahaan', 1)->groupBy('idBagian')->orderBy('idBagian')->get()->toArray();
         $pkwt = kontrakKaryawanModel::where('status', '1')->whereBetween('sampaiTanggal', [$notifikasi, $akhir])->count();
-        $sp = kontrakKaryawanModel::where('status', '1')->whereBetween('sampaiTanggal', [$notifikasi, $akhir])->count();
+        $sp = SPModel::where('status', '1')->whereBetween('sampaiTanggal', [$notifikasi, $akhir])->count();
         $data = [
             'title' => 'Sistem Manajemen Data Human Capital',
             'countEmployee' => $count,
